5826cdb85f39051d00bfdb60d99d79306c61f118
Fozilbek <bekhzodov7mu@gmail.com>
2024-04-06 22:02:10 +0500

    - update: refactor movement, clamp position in Border radius

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,40 +1,32 @@
-using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace TopDownHordes.Player
 {
     public class PlayerMovement : MonoBehaviour
     {
-        [Header("Components")]
-        [Required]
-        [SerializeField] private Rigidbody2D _rigidbody2D;
-        
         [Header("Stats")]
         [SerializeField] private float _movementSpeed = 5f;
+        
+        [SerializeField] private float _radius = 5f;
+        [SerializeField] private Vector3 _circlePos = Vector3.zero;
         
         private const float Offset = 270;
         
         private Vector2 _movementVelocity;
         private Vector2 _movementInput;
-        private Camera _camera;
         
         private float _horizontalMove;
         private float _verticalMove;
-        
-        private void Awake()
-        {
-            _camera = Camera.main;
-        }
 
         private void Update()
         {
             GetInput();
-            RotatePlayer();
-        }
-
-        private void FixedUpdate()
-        {
             MovePlayer();
+        }
+        
+        private void LateUpdate()
+        {
+            ClampToCircle();
         }
 
         private void GetInput()
@@ -45,15 +37,30 @@ namespace TopDownHordes.Player
 
         private void MovePlayer()
         {
-            _rigidbody2D.velocity = new Vector2(_horizontalMove * _movementSpeed, 
-                _verticalMove * _movementSpeed);
+            Vector3 movement = new(_horizontalMove, _verticalMove);
+            transform.position += movement * (_movementSpeed * Time.deltaTime);
+            
+            if (movement != Vector3.zero)
+            {
+                float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg + Offset;
+                transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            }
         }
 
-        private void RotatePlayer()
+        private void ClampToCircle()
         {
-            Vector3 difference = _camera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-            float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, 0f, rotZ + Offset);
+            float distanceFromCenter = (_circlePos - transform.position).magnitude;
+
+            if (distanceFromCenter > _radius)
+            {
+                Vector3 fromCenterToObject = transform.position - _circlePos;
+                transform.position = _circlePos + Vector3.ClampMagnitude(fromCenterToObject, _radius);
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(_circlePos, _radius);
         }
     }
 }
